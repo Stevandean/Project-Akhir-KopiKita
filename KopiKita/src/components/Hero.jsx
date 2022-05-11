a844e8ca1b36fd8a1eca703d175f4101921f1cb8
Stevandeann <stevandeanachmad1@gmail.com>
2022-05-12 06:55:56 +0700

    first commit

--- KopiKita/src/components/Hero.jsx
+++ KopiKita/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
             <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
                 {/* <h1 className='py-3 text-5xl md:text-7xl font-bold'>Kopi Kita</h1> */}
-                <h1 className='py-1 font-bold text-6 sm:text-4xl'>  Kopi Kita</h1>
+                <h1 className='py-1 font-bold text-6xl'>Kopi Kita</h1>
                 <p className='deskripsihome'>Dari Aku, Kopi dan Kamu menjadi Kita</p>
                 <button className='py-2 px-6 sm:w-[60%] my-1'>Pesan Sekarang</button>
             </div>
